d55b250afa63aa6b474b137d862b0550e0697336
yuchaz <erwin.yuchia.chen@gmail.com>
2016-03-08 09:51:34 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+import React from 'react';
+import AppRoot from './components/AppRoot'; // we'll create this next
+
+React.render(
+    <AppRoot />,
+    document.getElementById('app-root')
+);
